ab3e17fe8e36cb98c8ec1006478d067641e5f311
Tegar Risqy Yulian Santoso <72007606+TgrRys@users.noreply.github.com>
2023-08-30 22:35:22 +0700

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -32,7 +32,7 @@ export default function Home() {
         >
           <a
             className="rounded-[4px] border-2 border-accent px-7 py-5 text-accent transition-all duration-300 hover:bg-accent hover:bg-opacity-10"
-            href="https://drive.google.com/file/d/1zeoOnCjopbRGxCzcaAuTXPc18OpiAZZ6/view?usp=sharing"
+            href="https://drive.google.com/file/d/1iLiqtVR5NdLwmljkDmPT_St4PkaNNPlB/view?usp=sharing"
             target="_blank"
             rel="noreferrer"
           >
